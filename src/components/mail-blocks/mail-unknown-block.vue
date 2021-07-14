ed9a0f199bd1efd930b4eef788e9ec0828585b8f
ilya-guseynov <ilya.guseynov.self@gmail.com>
2021-07-14 15:36:23 +0300

    new: add mail-unknown-block

---
+++ src/components/mail-blocks/mail-unknown-block.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>Неизвестный блок типа {{ mailBlock.type }}</div>
+</template>
+
+<script>
+export default {
+  name: "mail-unknown-block",
+
+  props: {
+    mailBlock: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
