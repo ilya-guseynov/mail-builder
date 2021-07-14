10200a2125ab59d718ce6b5e1873e52fdd5db5df
ilya-guseynov <ilya.guseynov.self@gmail.com>
2021-07-14 14:18:43 +0300

    new: add mail-workspace and mail-block-container

---
+++ src/components/mail-block-container.vue
@@ -0,0 +1,179 @@
+<template>
+  <div class="mail-block-container">
+    <table style="border-spacing:0; width:100%;">
+      <tbody>
+        <tr>
+          <td
+            style="padding:0;"
+            class="mail-block-container__inner"
+            draggable="true"
+            @dragstart="startDragMailBlock($event)"
+            @drop="onDropMailBlockOnAnotherBlock($event)"
+            @dragover.prevent
+            @dragenter.prevent
+          >
+            <div class="mail-block-container__change-position">
+              <button
+                class="mail-block-container__change-position-button"
+                v-if="isFirstBlock"
+                @click="emitUpdateMailBlockPosition(mailBlock.position - 1)"
+              >↑</button>
+              <button
+                class="mail-block-container__change-position-button"
+                v-if="isLastBlock"
+                @click="emitUpdateMailBlockPosition(mailBlock.position + 1)"
+              >↓</button>
+            </div>
+            <div class="mail-block-container__remove-button-container">
+              <button class="mail-block-container__remove-button" @click="emitRemoveMailBlock">X</button>
+            </div>
+            <div>{{ mailBlock.type }}</div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "mail-block-container",
+
+  emits: [
+    "remove-mail-block",
+    "update-mail-block-position",
+  ],
+
+  props: {
+    mailBlock: {
+      type: Object,
+      required: true,
+    },
+
+    isFirstBlock: {
+      type: Boolean,
+      required: true,
+    },
+
+    isLastBlock: {
+      type: Boolean,
+      required: true,
+    },
+  },
+
+  methods: {
+    /**
+     * Emits to parent component, that current mail block must be removed.
+     */
+    emitRemoveMailBlock() {
+      this.$emit("remove-mail-block");
+    },
+
+    /**
+     * Emits to parent component, that current component position must be changed to new one.
+     *
+     * @param { number } newPosition
+     */
+    emitUpdateMailBlockPosition(newPosition) {
+      this.$emit("update-mail-block-position", this.mailBlock.id, newPosition);
+    },
+
+    /**
+     * Handles event, when mail block is draged by user.
+     *
+     * @param { object } event
+     */
+    startDragMailBlock(event) {
+      event.dataTransfer.dropEffect = "move";
+      event.dataTransfer.effectAllowed = "move";
+      event.dataTransfer.setData("text/plain", this.mailBlock.id);
+    },
+
+    /**
+     * Handles event, when mail block is droped by user.
+     *
+     * @param { object } event
+     */
+    onDropMailBlockOnAnotherBlock(event) {
+      const dropedMailBlockId = event.dataTransfer.getData("text");
+      this.$emit("update-mail-block-position", dropedMailBlockId, this.mailBlock.position);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.mail-block-container {
+  &__inner {
+    position: relative;
+
+    &:hover .mail-block-container__remove-button {
+      opacity: 1;
+    }
+
+    &:hover .mail-block-container__change-position {
+      opacity: 1;
+    }
+  }
+
+  &__remove-button-container {
+    position: absolute;
+    left: 0;
+    transform: translateX(-100%);
+    padding-right: 10px;
+    height: 100%;
+    display: flex;
+    align-items: center;
+  }
+
+  &__remove-button {
+    height: 20px;
+    font-size: 20px;
+    width: 22px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: none;
+    outline: none;
+    cursor: pointer;
+    background: none;
+    color: black;
+    opacity: 0;
+    transition: all .2s;
+
+    &:hover {
+      background: rgb(236, 236, 236);
+    }
+  }
+
+  &__change-position {
+    position: absolute;
+    left: 0;
+    transform: translateX(-100%);
+    padding-right: 40px;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    opacity: 0;
+    transition: all .2s;
+  }
+
+  &__change-position-button {
+    height: 20px;
+    font-size: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: none;
+    outline: none;
+    cursor: pointer;
+    background: none;
+    color: black;
+    transition: all .2s;
+
+    &:hover {
+      background: rgb(236, 236, 236);
+    }
+  }
+}
+</style>
---
+++ src/components/mail-workspace.vue
@@ -0,0 +1,127 @@
+<template>
+  <div class="mail-workspace mail-workspace--no-blocks" v-if="!hasMailBlocks">
+    <new-block-creator :position="0" @create-mail-block="emitAddMailBlock"></new-block-creator>
+    <div class="mail-workspace__no-blocks-message">Создайте первый блок выше</div>
+  </div>
+  <div class="mail-workspace" v-else>
+    <table style="border-spacing:0; width:100%; max-width:600px;">
+      <tbody>
+        <tr>
+          <td style="padding:0;">
+            <div class="mail-workspace__block" v-for="mailBlock in orderedMailBlocks" :key="mailBlock.id">
+              <new-block-creator :position="mailBlock.position" @create-mail-block="emitAddMailBlock"></new-block-creator>
+              <mail-block-container
+                :mail-block="mailBlock"
+                :is-first-block="mailBlock.position > 0"
+                :is-last-block="mailBlock.position < mailBlocksCount - 1"
+                @remove-mail-block="emitRemoveMailBlock(mailBlock.position)"
+                @update-mail-block-position="emitUpdateMailBlockPosition"
+                @update-mail-block-content="emitUpdateMailBlockContent"
+              ></mail-block-container>
+            </div>
+            <new-block-creator :position="mailBlocksCount" @create-mail-block="emitAddMailBlock"></new-block-creator>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import NewBlockCreator from "./new-block-creator";
+import MailBlockContainer from "./mail-block-container";
+
+export default {
+  name: "mail-workspace",
+
+  components: {
+    NewBlockCreator,
+    MailBlockContainer,
+  },
+
+  props: {
+    mailBlocks: {
+      type: Array,
+      required: true,
+    },
+  },
+
+  emits: [
+    "add-mail-block",
+    "remove-mail-block",
+    "update-mail-block-content",
+    "update-mail-block-position",
+  ],
+
+  computed: {
+    /**
+     * Returns mail blocks sorted by their positions.
+     */
+    orderedMailBlocks() {
+      return [...this.mailBlocks].sort(
+        (firstMailBlock, secondMailBlock) => (firstMailBlock.position - secondMailBlock.position)
+      );
+    },
+
+    /**
+     * Returns status if there are at least one mail block.
+     */
+    hasMailBlocks() {
+      return this.mailBlocks.length > 0;
+    },
+
+    /**
+     * Returns amount of mail blocks.
+     */
+    mailBlocksCount() {
+      return this.mailBlocks.length;
+    },
+  },
+
+  methods: {
+    /**
+     * Emits to parent component, that mail block with provided type must be created on provided position.
+     */
+    emitAddMailBlock(newMailBlockType, newMailBlockPosition) {
+      this.$emit("add-mail-block", newMailBlockType, newMailBlockPosition);
+    },
+
+    /**
+     * Emits to parent component, that mail block with provided position must be removed.
+     */
+    emitRemoveMailBlock(blockToRemovePosition) {
+      this.$emit("remove-mail-block", blockToRemovePosition);
+    },
+
+    /**
+     * Emits to parent component, that provided mail block content must be changed to new content.
+     */
+    emitUpdateMailBlockContent(blockToUpdate, newContent) {
+      this.$emit("update-mail-block-position", blockToUpdate, newContent);
+    },
+
+    /**
+     * Emits to parent component, that provided mail block position must be changed to new position.
+     */
+    emitUpdateMailBlockPosition(blockToUpdateId, newPosition) {
+      const blockToUpdate = this.mailBlocks.find(mailBlock => mailBlock.id === blockToUpdateId);
+      this.$emit("update-mail-block-position", blockToUpdate, newPosition);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.mail-workspace {
+  width: 100%;
+  margin-top: 300px;
+
+  &__no-blocks-message {
+    text-align: center;
+  }
+
+  &__block {
+    width: 100%;
+  }
+}
+</style>
